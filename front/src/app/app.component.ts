c2ce947f05fa58336dbcd69f3dd6319fe968c7d7
ComaSpace <kijucrolandas@gmail.com>
2024-06-11 17:54:05 +0300

    CRUD product

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CalculatorComponent]
+    imports: [RouterOutlet, CalculatorComponent, NavigationComponent, CommonModule]
 })
 export class AppComponent {
   title = 'front';
